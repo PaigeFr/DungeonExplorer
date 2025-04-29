37a2f4c26ea8e27c30b118511316bf9fc516afb2
PaigeFr <paige.francome0@gmail.com>
2025-04-29 10:38:56 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace DungeonExplorer
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Tests.RunTests();
+
+            // Runs tests
+            var testingPlayer = new PlayerTests();
+            testingPlayer.RunTests();
+            Console.WriteLine("Player testing completed.");
+
+            var testingPuzzle = new PuzzleTests();
+            testingPuzzle.RunTests();
+            Console.WriteLine("Puzzle testing completed.");
+
+            var testingStatistics = new StatisticsTests();
+            testingStatistics.RunTests();
+            Console.WriteLine("Statistics testing completed.");
+
+            // Starts the game
             Game game = new Game();
             game.Start();
             Console.WriteLine("Waiting for your Implementation");
